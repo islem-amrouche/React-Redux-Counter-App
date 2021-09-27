ab7f43660f5f769ae241f2c734a56ea068a58829
Islem <islem@islem.com>
2021-09-27 12:50:46 +0100

    Increment Decrement Double Reset Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import {increment, decrement, login, reset, double} from './actions'
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch= useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1>Redux Counter App</h1>
+      <h1>Counter is {counter}</h1>
+      <button className="btn btn-lg btn-info" onClick={()=>dispatch(increment())}>+</button>
+      <button className="btn btn-lg btn-dark ml-1" onClick={()=>dispatch(decrement())}>-</button>
+      <button className="mybtn btn btn-lg btn-success mt-2" onClick={()=>dispatch(double())}>Double</button>
+      <button className="mybtn btn btn-lg btn-danger mt-2" onClick={()=>dispatch(reset())}>RESET</button>
+
+      <button className={`mybtn btn btn-lg mt-3 ${isLogged ? 'btn-danger':'btn-primary'}`} onClick={()=>dispatch(login())}>
+        {isLogged? <p>LOGOUT</p>: <p>LOGIN</p>}
+      </button>
+      <br/>
+      {isLogged? <h3>You're logged in</h3>:''}
     </div>
   );
 }
---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+export const increment =()=>{
+  return {
+    type: "INCREMENT",
+  }
+}
+export const decrement =()=>{
+  return {
+    type: "DECREMENT"
+  }
+}
+export const reset =()=>{
+  return {
+    type: "RESET"
+  }
+}
+export const double =()=>{
+  return {
+    type: "DOUBLE"
+  }
+}
+export const login =()=>{
+  return {
+    type: "SIGN_IN"
+  }
+}
